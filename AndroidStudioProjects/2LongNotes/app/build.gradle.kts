893cdfb7b35d0ba51013a6674b1b712fb83aaf8d
Devonte M Longmire <77998284+Longyde@users.noreply.github.com>
2025-11-05 21:48:36 -0800

    Add files via upload

--- AndroidStudioProjects/2LongNotes/app/build.gradle.kts
+++ AndroidStudioProjects/2LongNotes/app/build.gradle.kts
@@ -64,6 +64,12 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.3.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.material:material-icons-extended:1.6.7")
+    
+    // Calendar
+    implementation("io.github.boguszpawlowski.composecalendar:composecalendar:1.1.0")
+
+    // Splash Screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     // Compose core
     implementation("androidx.activity:activity-compose:1.9.3")
